3641c1c584ed537b8de530caeee7984e5388bf53
MehSwitch <aymehaiDev@protonmail.com>
2018-06-13 14:35:36 -0400

    Added ask again functionality

--- app/src/main/java/com/aymen/magic8ball/Magic8BallResultsActivity.kt
+++ app/src/main/java/com/aymen/magic8ball/Magic8BallResultsActivity.kt
@@ -1,7 +1,9 @@
 package com.aymen.magic8ball
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.magic_eightball_result.*
 import java.text.DecimalFormat
 
@@ -12,11 +14,19 @@ class Magic8BallResultsActivity : AppCompatActivity() {
         setContentView(R.layout.magic_eightball_result)
 
         show8BallResults()
+        setOnClickListenerForButton()
     }
 
 
     private fun show8BallResults(){
         var formatted = getIntent().getExtras().getString("ballResults")
         txt8BallResults.setText(formatted.toString())
+    }
+
+    private fun setOnClickListenerForButton(){
+        btnAskAgain.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
